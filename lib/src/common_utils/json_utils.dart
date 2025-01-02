aa6058055a0519ff4eda05abc7b70721e844e707
quanptit <quanptit410@gmail.com>
2025-01-02 19:18:16 +0700

    u

--- lib/src/common_utils/json_utils.dart
+++ lib/src/common_utils/json_utils.dart
@@ -3,14 +3,19 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 
 class JsonUtils {
-
-///fromJson is generate function. EX: VideoObject.fromJson
-///ex: List<Obj> objs = parserJsonToArray(jsonText, Obj.fromJson)
+  ///fromJson is generate function. EX: VideoObject.fromJson
+  ///ex: List<Obj> objs = parserJsonToArray(jsonText, Obj.fromJson)
   static Future<List<T>> parserJsonToArray<T>(String responseBody, T Function(Map<String, dynamic> json) fromJson) {
-    return compute(
-      (String message) => (jsonDecode(message) as List).map((jsonObj) => fromJson(jsonObj)).toList(),
-      responseBody,
-    );
+    try {
+      return compute(
+        (String message) => (jsonDecode(message) as List).map((jsonObj) => fromJson(jsonObj)).toList(),
+        responseBody,
+      );
+    } on Exception catch (e) {
+      throw e;
+    } catch (e) {
+      throw Exception(e.toString());
+    }
   }
 
   ///fromJson is generate function. EX: VideoObject.fromJson
